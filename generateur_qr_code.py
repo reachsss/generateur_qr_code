6c43d138a8ebfb4bfd7e2aab0659f1a20698b0fc
reach <49105956+reachsss@users.noreply.github.com>
2026-08-12 13:35:46 +0200

    Add files via upload

---
+++ generateur_qr_code.py
@@ -0,0 +1,16 @@
+import qrcode
+
+def generer_qr_code(lien, nom_fichier):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4
+    )
+    qr.add_data(lien)
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save(nom_fichier)
+
+#Executez la fonction generer_qr_code avec le lien et le nom du fichier pour obtenir votre QR Code ! 
+#lien et nom_fichier doivent etre du texte (string)
